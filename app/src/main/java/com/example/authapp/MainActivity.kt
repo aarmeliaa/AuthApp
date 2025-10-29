bf0ad2532709ee81e117c5ebd8fb1d8b4bf718ba
aarmeliaa <azzahraarmrlia14@gmail.com>
2025-10-29 13:33:24 +0700

    mengisikan logic untuk login, register, dan main(dashboard).

--- app/src/main/java/com/example/authapp/MainActivity.kt
+++ app/src/main/java/com/example/authapp/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.example.authapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.authapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var prefManager: PrefManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        prefManager = PrefManager.getInstance(this)
+        checkLoginStatus()
+        with(binding) {
+            txtUsername.text = prefManager.getUsername()
+            btnLogout.setOnClickListener {
+                prefManager.setLoggedIn(false)
+                startActivity(Intent(
+                    this@MainActivity,
+                    LoginActivity::class.java))
+                finish()
+            }
+            btnClear.setOnClickListener {
+                prefManager.clear()
+                finish()
+            }
+        }
+    }
+    fun checkLoginStatus() {
+        val isLoggedIn = prefManager.isLoggedIn()
+        if (!isLoggedIn) {
+            startActivity(Intent(
+                this@MainActivity,
+                LoginActivity::class.java))
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/authapp/RegisterActivity.kt
+++ app/src/main/java/com/example/authapp/RegisterActivity.kt
@@ -1,20 +1,65 @@
 package com.example.authapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.authapp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var prefManager: PrefManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        prefManager = PrefManager.getInstance(this)
+        with(binding) {
+            btnRegister.setOnClickListener {
+                val username = edtUsername.text.toString()
+                val password = edtPassword.text.toString()
+                val confirmPassword = edtPasswordConfirm.text.toString()
+                if (username.isEmpty() || password.isEmpty() ||
+                    confirmPassword.isEmpty()) {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Mohon isi semua data",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else if (password != confirmPassword) {
+                    Toast.makeText(this@RegisterActivity, "Password tidak sama",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    prefManager.saveUsername(username)
+                    prefManager.savePassword(password)
+                    prefManager.setLoggedIn(true)
+                    checkLoginStatus()
+                }
+            }
+            txtLogin.setOnClickListener {
+                startActivity(
+                    Intent(
+                        this@RegisterActivity,
+                        LoginActivity::class.java
+                    )
+                )
+            }
+        }
+    }
+    private fun checkLoginStatus() {
+        val isLoggedIn = prefManager.isLoggedIn()
+        if (isLoggedIn) {
+            Toast.makeText(this@RegisterActivity, "Registrasi berhasil",
+                Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this@RegisterActivity, MainActivity::class.java))
+            finish()
+        } else {
+            Toast.makeText(this@RegisterActivity, "Registrasi gagal",
+                Toast.LENGTH_SHORT).show()
         }
     }
 }
